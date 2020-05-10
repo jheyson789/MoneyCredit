c5d9d3c1c42ab235a5c9ec958ea73c151ae1d30c
Jheyson Zurita Soria <jheysoria20@gmail.com>
2020-05-10 00:03:29 -0400

    Boton de eliminar en el inicio

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PrestamosService } from '../../services/prestamos.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inicio',
@@ -20,7 +21,6 @@ export class InicioComponent implements OnInit {
   }
 
   mostrarPrestamos() {
-
     this.servicioPrestamo.getProjects().subscribe(
       response => {
         if(response.prestamos) {
@@ -28,10 +28,45 @@ export class InicioComponent implements OnInit {
         }
       },
       error => {
-        
+        Swal.fire({
+          icon:'error',
+          title: 'Opops...',
+          text: `No se pudo conectar a la base de datos`
+        })
       }
 
     )
+
+  }
+
+  eliminarPrestamo(id: String, i: number) {
+    Swal.fire({
+      title: '¿Estas Seguro?',
+      text: `Estas seguro que desea eliminar de los prestamos`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then( resp => {
+      if(resp.value){
+      this.datos.splice(i,1);
+      this.servicioPrestamo.eliminarPrestamo(id).subscribe(
+        prestamo=> {
+          Swal.fire(
+            'Se elimino Correctamente',
+            'Que tengas un buen día',
+            'success'
+          )
+        },
+        error => {
+          Swal.fire({
+            icon:'error',
+            title: 'Opops...',
+            text: `No se encontro el id ${id}`
+          })
+        }
+      )
+      }
+    })
 
   }
 
